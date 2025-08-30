cf11b77eef70349d886100ec43b23ab1ca582bcb
hieuvo0ampitech <hieuvo.ampitech@gmail.com>
2025-08-30 10:41:54 +0700

    refactor: improve code structure and readability

--- lib/app/modules/user/views/user_view.dart
+++ lib/app/modules/user/views/user_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:vmh_application/app/helpers/constants.dart';
+import 'package:vmh_application/app/helpers/responsive.dart';
 
 import '../controllers/user_controller.dart';
 
@@ -19,69 +20,123 @@ class UserView extends GetView<UserController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Obx(
-          () => Padding(
-            padding: const EdgeInsets.all(defaultPadding * 3),
-            child: DataTable2(
-              columns: [
-                DataColumn(
-                  label: Text(
-                    'Tên',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  columnWidth: FlexColumnWidth(15),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Email',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  columnWidth: FlexColumnWidth(15),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Role',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  columnWidth: FlexColumnWidth(2),
-                ),
-                DataColumn(
-                  label: Center(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Tạo người dùng mới',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+        child: Padding(
+          padding: const EdgeInsets.all(defaultPadding * 3),
+          child: Responsive.isDesktop(context) || Responsive.isTablet(context)
+              ? LargeTable(controller: controller)
+              : SmallTable(controller: controller),
+        ),
+      ),
+    );
+  }
+}
+
+class SmallTable extends StatelessWidget {
+  const SmallTable({super.key, required this.controller});
+
+  final UserController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => DataTable2(
+        columns: [
+          DataColumn2(
+            label: Text('Tên', style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          DataColumn2(
+            label: Center(
+              child: ElevatedButton(onPressed: () {}, child: Icon(Icons.add)),
+            ),
+          ),
+        ],
+        rows: controller.users
+            .map(
+              (user) => DataRow(
+                cells: [
+                  DataCell(Text(user.fullName)),
+                  DataCell(
+                    Center(
+                      child: IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          // Handle edit action
+                        },
                       ),
                     ),
                   ),
-                  columnWidth: FlexColumnWidth(2),
-                ),
-              ],
-              rows: controller.users
-                  .map(
-                    (user) => DataRow(
-                      cells: [
-                        DataCell(Text(user.fullName)),
-                        DataCell(Text(user.email)),
-                        DataCell(Text(user.role)),
-                        DataCell(
-                          Center(
-                            child: IconButton(
-                              icon: Icon(Icons.edit),
-                              onPressed: () {
-                                // Handle edit action
-                              },
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                  .toList(),
-            ),
+                ],
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class LargeTable extends StatelessWidget {
+  const LargeTable({super.key, required this.controller});
+
+  final UserController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => DataTable2(
+        columns: [
+          DataColumn2(
+            label: Text('Tên', style: TextStyle(fontWeight: FontWeight.bold)),
+            size: ColumnSize.M,
           ),
-        ),
+          DataColumn2(
+            label: Text('Email', style: TextStyle(fontWeight: FontWeight.bold)),
+            size: ColumnSize.L,
+          ),
+          Responsive.isDesktop(context)
+              ? DataColumn2(
+                  label:
+                      Text('Vai trò', style: TextStyle(fontWeight: FontWeight.bold)),
+                  size: ColumnSize.S,
+                )
+              : DataColumn2(
+                  label: Text('Vai trò',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  size: ColumnSize.M,
+                ),
+          DataColumn2(
+            label: Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Tạo người dùng mới',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            fixedWidth: 200,
+          ),
+        ],
+        rows: controller.users
+            .map(
+              (user) => DataRow(
+                cells: [
+                  DataCell(Text(user.fullName)),
+                  DataCell(Text(user.email)),
+                  DataCell(Text(user.role)),
+                  DataCell(
+                    Center(
+                      child: IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          // Handle edit action
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+            .toList(),
       ),
     );
   }
